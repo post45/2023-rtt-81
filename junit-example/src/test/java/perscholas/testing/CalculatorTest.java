b263551f1826a580a84743acd5f327c98e5f6c8c
archie9992 <bashkuresh92@gmail.com>
2023-10-31 05:57:59 -0700

    Unit5 test calculator

--- junit-example/src/test/java/perscholas/testing/CalculatorTest.java
+++ junit-example/src/test/java/perscholas/testing/CalculatorTest.java
@@ -1,27 +1,66 @@
- package perscholas.testing;
+package perscholas.testing;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.ParameterizedTest;
 
 public class CalculatorTest {
 
-	@Test
-	public void addtionTest() {
+	@ParameterizedTest
+	@CsvSource({ "5,5,10", "15,15,30", "5.5,5.5,11.0", })
+	public void addtionTest(double a, double b, double expected) {
 		Calculator c = new Calculator();
-		double expected =10;
-		double actual = c.add(5, 5);
-		Assertions.assertEquals(expected,actual);
-		
-		Assertions.assertEquals(15, c.add(7.5, 7.5));
+		// double expected =10;
+		double actual = c.add(a, b);
+		Assertions.assertEquals(expected, actual);
 	}
-	
-	@Test
-	public void substractionTest() {
+
+	@ParameterizedTest
+	@CsvSource({ "5,5,11", "16,15,30", "5.5,7.5,11.0", })
+	public void addtionErrorTest(double a, double b, double expected) {
 		Calculator c = new Calculator();
-		double expected =0;
-		double actual = c.substract(5, 5);
-		Assertions.assertEquals(expected,actual);
-		
-		Assertions.assertEquals(15, c.substract(30.5, 15.5));
+		// double expected =10;
+		double actual = c.add(a, b);
+		Assertions.assertNotEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "5,5,0", "16,15,1", "5.5,7.5,-2", })
+	public void substractionTest(double a, double b, double expected) {
+		Calculator c = new Calculator();
+		// double expected =10;
+		double actual = c.substract(a, b);
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "5,5,1", "16,4,4", "5.5,5.5,1", })
+	public void divideTest(double a, double b, double expected) {
+		Calculator c = new Calculator();
+		// double expected =10;
+		double actual = c.divide(a, b);
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "5,0,1", "0.5,0,0", })
+	public void divideExceptionTest(double a, double b, double expected) {
+		Calculator c = new Calculator();
+		// double expected =10;
+		double actual = c.divide(a, b);
+		Assertions.assertDoesNotThrow(() -> {
+		}, "");
+		Assertions.assertEquals(Double.POSITIVE_INFINITY, actual);
+
+	}
+
+	@Test
+	public void divideNaNTest() {
+		Calculator c = new Calculator();
+		// double expected =10;
+		double actual = c.divide(0, 0);
+		Assertions.assertEquals(Double.NaN, actual);
+
 	}
 }
